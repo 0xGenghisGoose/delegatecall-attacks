ecf236552f6624669d12ad1f08cbf65b976cca75
Genghis Goose <86017946+0xGenghisGoose@users.noreply.github.com>
2022-05-10 12:38:02 -0500

    helper, attack contracts written with test

---
+++ test/attack.js
@@ -0,0 +1,30 @@
+const { expect } = require('chai');
+const { BigNumber } = require('ethers');
+const { ethers, waffle } = require('hardhat');
+
+describe('Attack', function () {
+	it('Should change the owner of the Good contract', async function () {
+		// Deploy the three contracts
+		const helperContract = await ethers.getContractFactory('Helper');
+		const helper = await helperContract.deploy();
+		await helper.deployed();
+		console.log("Helper contract's address:", helper.address);
+
+		const goodContract = await ethers.getContractFactory('Good');
+		const good = await goodContract.deploy();
+		await good.deployed();
+		console.log("Good contract's address:", good.address);
+
+		const attackContract = await ethers.getContractFactory('Attack');
+		const attack = await attackContract.deploy(goodContract.address);
+		await attack.deployed();
+		console.log("Attack contract's address:", attack.address);
+
+		// Start the attack
+		let tx = await attack.attack();
+		await tx.wait();
+
+		// Check to see if Good contract's owner address has changed to Attack contract address
+		expect(await good.owner()).to.equal(attack.address);
+	});
+});
